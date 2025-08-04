eeafe6f52e7c457a61bf75cf1ed0ef14b270ca5f
Kride024 <kritiyadavcoding@gmail.com>
2025-08-04 22:30:51 +0530

    first commit

---
+++ hello-world/file.js
@@ -0,0 +1,34 @@
+// const fs = require("fs");
+// Sync...
+// fs.writeFileSync('./test.txt', 'Hey There');
+
+//Async
+const fs = require("fs");
+
+//Sync..
+// fs.writeFileSync("./test.txt","Hello World");
+
+//Async..
+// fs.writeFile("./test.txt", "Hello World !! Async", (err) => {})
+
+
+//Sync : return something
+  const result = fs.readFileSync("./contact.txt","utf-8");
+console.log(result);
+//Asnc : Does'nt return something
+  fs.readFile("./contact.txt", "utf-8",(err,result)=>{
+    if(err){
+      console.log("Error:",err);
+    }
+  else{
+ console.log(result);
+  }
+  });
+  
+//! Append
+ fs.appendFileSync("./test.txt",`${Date.now()} Hey There\n`); //Does'nt overwrite but write data after existing data
+
+fs.cpSync("./test.txt","./copy.txt");//make copy file of available file
+fs.unlinkSync("./copy.txt");//delete any file
+
+
---
+++ hello-world/hello.js
@@ -0,0 +1,22 @@
+const math = require('./math')
+// const {add , sub}= require('./math'); //destructuring
+
+// for current directory write like ./math.js and built in directly write name
+console.log("Hey! JS Here");
+
+// console.log(alert("Hey"));
+//UI, dom  related work removed here, core functionality only there which we need on server side
+// console.log(math.sub1(2,3)); //{}
+// console.log(add(2,3));
+
+//! Web servers
+// const server = require("http") //web server
+//! file system
+// const fileSystem=require("fs");//file handling , built in node 
+
+//as it is math then search in node_modules folder
+//./math then search in folder/current directory
+//! crypto
+//you have to do hashing, encryption, decryption there it use
+//! In java
+// we make package then we import
