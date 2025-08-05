203cc227ade4547160d651b40503019b30b2fc9a
Kride024 <kritiyadavcoding@gmail.com>
2025-08-05 16:24:49 +0530

    commit

--- hello-world/file.js
+++ hello-world/file.js
@@ -4,6 +4,8 @@
 
 //Async
 const fs = require("fs");
+const os = require("os");
+console.log(os.cpus().length);
 
 //Sync..
 // fs.writeFileSync("./test.txt","Hello World");
@@ -13,36 +15,47 @@ const fs = require("fs");
 
 
 //Sync : return something
-  const result = fs.readFileSync("./contact.txt","utf-8");
-console.log(result);
-//Asnc : Does'nt return something
-  fs.readFile("./contact.txt", "utf-8",(err,result)=>{
-    if(err){
-      console.log("Error:",err);
-    }
-  else{
- console.log(result);
-  }
-  });
+//   const result = fs.readFileSync("./contact.txt","utf-8");
+// console.log(result);
+// //Asnc : Does'nt return something
+//   fs.readFile("./contact.txt", "utf-8",(err,result)=>{
+//     if(err){
+//       console.log("Error:",err);
+//     }
+//   else{
+//  console.log(result);
+//   }
+//   });
   
 //! Append
- fs.appendFileSync("./test.txt",`${Date.now()} Hey There\n`); //Does'nt overwrite but write data after existing data
+//  fs.appendFileSync("./test.txt",`${Date.now()} Hey There\n`); //Does'nt overwrite but write data after existing data
 
-fs.cpSync("./test.txt","./copy.txt");//make copy file of available file
-fs.unlinkSync("./copy.txt");//delete any file
+// fs.cpSync("./test.txt","./copy.txt");//make copy file of available file
+// fs.unlinkSync("./copy.txt");//delete any file
 
 
-console.log(fs.statSync("./test.txt").isFile());
+// console.log(fs.statSync("./test.txt").isFile());
 // fs.mkdirSync("my-docs");//make directory
 // fs.rmdirSync("my-docs");//remove directory
 
-fs.mkdirSync("my-docs/a/b",{recursive:true});
+// fs.mkdirSync("my-docs/a/b",{recursive:true});
 
 //in normal js there is nothing like file handling file thing due to security purposes.
 
-//Async() Recommended
+//!Async() Recommended (We should always use non-blocking request)
 // const fs = require('fs');
 // fs.readFile('file.txt', 'utf8', (err, data) => {
 //     if (err) throw err;
 //     console.log(data); // Non-blocking
 // });
+
+//! Software Architecture
+
+// console.log("1");
+// const result = fs.readFile("contact.txt","utf-8",(err,result)=>{
+//   console.log(result);
+//   });
+// //  console.log(result);
+//  console.log("2");
+
+
