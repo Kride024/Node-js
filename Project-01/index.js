c6b10b15f9d0a17e7db9da81fa61c58b5b32bf65
Kride024 <kritiyadavcoding@gmail.com>
2025-08-12 17:10:52 +0530

    add

--- Project-01/index.js
+++ Project-01/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
-
+const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
+
+
+//Routes
+
+app.get("/api/users",(req,res)=>{
+    return res.json (users);
+});
+
+app.get('/users',(req,res)=>{
+const html = `
+<ul>
+${users.map((user)=>`<li>${user.first_name}</li>`).join('')}
+</ul>`
+return res.send(html);
+});
+// Note: To remove , use .join('') instead of .join(', ') 
 
 app.listen(PORT,()=>console.log(`server started at Port ${PORT}`));
