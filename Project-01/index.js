908cd35cf48c5bcde8560e54ab033d920c451ddf
Kride024 <kritiyadavcoding@gmail.com>
2025-08-12 17:54:16 +0530

    add

--- Project-01/index.js
+++ Project-01/index.js
@@ -6,9 +6,6 @@ const PORT = 8000;
 
 //Routes
 
-app.get("/api/users",(req,res)=>{
-    return res.json (users);
-});
 
 app.get('/users',(req,res)=>{
 const html = `
@@ -18,5 +15,40 @@ ${users.map((user)=>`<li>${user.first_name}</li>`).join('')}
 return res.send(html);
 });
 // Note: To remove , use .join('') instead of .join(', ') 
+
+// for alexa and smart devices request for json , its our hybrid server
+app.get("/api/users",(req,res)=>{
+    return res.json (users);
+});
+
+app.route("/api/users/:id").get((req,res)=>{
+    const id = Number(req.params.id); //req.params: route parameters
+    const user = users.find((user)=>user.id==id);
+    return res.json(user);
+})
+.patch((req,res)=>{
+    //edit user with ID
+    return res.json({status:pending});
+})
+.delete((req,res)=>{
+       //delete user with ID
+    return res.json({status:pending});
+})
+
+
+app.post("/api/users",(req,res)=>{
+    // TODO: Create new User
+    return res.json({status: "pending"});
+});
+
+// app.patch("/api/users/:id",(req,res)=>{
+//     // TODO: Edit User with ID
+//     return res.json({status: "pending"});
+// });
+
+// app.Delete("/api/users/:id",(req,res)=>{
+//     // TODO: DELETE user with ID
+//     return res.json({status: "pending"});
+// });
 
 app.listen(PORT,()=>console.log(`server started at Port ${PORT}`));
